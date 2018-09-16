b0a957bd919665c1e6c139f262a9f484b8a35d04
jamesk14022 <jameskingsbury919@gmail.com>
2018-09-17 00:59:12 +0100

    #7 map views

--- server/routes/loader.js
+++ server/routes/loader.js
@@ -13,7 +13,7 @@ import Loadable from 'react-loadable';
 
 // Our store, entrypoint, and manifest
 import createStore from './../../src/store';
-import App from './../../src/app/app';
+import App from './../../src/app/App';
 import manifest from './../../build/asset-manifest.json';
 
 // LOADER
